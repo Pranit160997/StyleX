37c5d7bb67eb1247df3c362f6113ee85ded45b61
Pranit160997 <pranitkumbhar19@gmail.com>
2024-05-10 20:47:04 -0400

    edited index.js for cors-added frontend domain

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-import cors from 'cors'; 
+import cors from 'cors';
 import path from 'path';
 dotenv.config();
 
@@ -25,7 +25,11 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: 'https://style-x.vercel.app/', // Replace with your frontend domain
+  methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
+  credentials: true, // Allow cookies
+}));
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server is running!');
