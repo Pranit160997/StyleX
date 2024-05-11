ed6c8d4ada70d625e01e8220d55122d0ea5a43d4
Pranit160997 <pranitkumbhar19@gmail.com>
2024-05-11 01:43:33 -0400

    edited index.js for app.use(cors) added dev localhost as well, replaced https to http

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 app.use(cookieParser());
 app.use(cors({
-  origin: ['https://style-x.vercel.app', 'https://localhost:5173'], // Replace with your frontend domain
+  origin: ['https://style-x.vercel.app', 'http://localhost:5173'], // Replace with your frontend domain
   methods: 'GET,POST,PUT,DELETE', // Allowed HTTP methods
   credentials: true, // Allow cookies
 }));
