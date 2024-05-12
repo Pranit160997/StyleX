ef35a3118e6fcc1a1d35497bbe064fac483c9e53
Pranit160997 <pranitkumbhar19@gmail.com>
2024-05-12 14:13:56 -0400

    commited new changes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -17,15 +17,17 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
+  console.log(email)
+  console.log(password)
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, 'User not found!'));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
-    const token = jwt.sign({ id: validUser._id }, "abcdefg12345");
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
     res
-      .cookie('access_token', token, { httpOnly: true })
+      .cookie('access_token', token, { httpOnly: true, /* sameSite: "none", secure: true */ })
       .status(200)
       .json(rest);
   } catch (error) {
@@ -40,7 +42,7 @@ export const google = async (req, res, next) => {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
       res
-        .cookie('access_token', token, { httpOnly: true })
+        .cookie('access_token', token, { httpOnly: true, sameSite: "none", /* secure: true */ })
         .status(200)
         .json(rest);
     } else {
@@ -60,7 +62,7 @@ export const google = async (req, res, next) => {
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = newUser._doc;
       res
-        .cookie('access_token', token, { httpOnly: true })
+        .cookie('access_token', token, { httpOnly: true, sameSite: "none", /* secure: true */ })
         .status(200)
         .json(rest);
     }
--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -3,7 +3,8 @@ import { errorHandler } from './error.js';
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-
+  console.log(token)
+  console.log(req.cookies)
   if (!token) return next(errorHandler(401, 'Unauthorized'));
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
