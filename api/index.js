b56173f4ccc6069434f7e7a0cac64ea1342aabfe
Pranit160997 <pranitkumbhar19@gmail.com>
2024-07-23 22:29:53 -0400

    added path_resolver folder in api and modified api's index.js

--- api/index.js
+++ api/index.js
@@ -39,6 +39,12 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(__dirname, '/path_resolver/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'path_resolver', 'dist', 'index.html'));
+})
+
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
